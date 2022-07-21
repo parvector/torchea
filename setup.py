4a0ffe26598f85fc027fa241920ca0fbfb78a3c2
Parviz Mirzoev <parvector@yandex.com>
2022-07-22 00:01:39 +0300

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torchea",
+    version="0.1",
+    author="Mirzoev Parviz",
+    author_email="parvector@yandex.com",
+    description="Training and construction of torch models based on evolutionary algorithms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/parvector/torchea",
+    project_urls={
+        "Bug Tracker": "https://github.com/parvector/torchea/issues",
+    },
+    packages=setuptools.find_packages(where="."), #
+    python_requires=">=3.8.10", 
+    install_requires = ["tqdm==4.64.0"]
+)
