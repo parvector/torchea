c96c52b89969f1fb172299af138bcd086c23b2cc
Parviz Mirzoev <parvector@yandex.com>
2022-09-25 12:44:34 +0300

    add base idvdl api

--- torchea/base.py
+++ torchea/base.py
@@ -1,8 +1,7 @@
 import torch, time, hashlib
 from torch import nn
 from enum import Enum
-from functools import partial
-from collections import Iterable
+from collections.abc import Iterable
 
 
 
@@ -13,7 +12,13 @@ class Task(Enum):
 
 
 class BaseIndvdl(nn.ModuleList):
-    def __init__(self, birthtime=time.time(), name=None) -> None:
+    def __init__(self, target_tensors="all", birthtime=time.time(), name=None) -> None:
+        """
+        Args:
+            target_tensors(list): The list of indexes of tensors on which evolutionary operations will be applied. If the list is "all", the operations will be performed on all tensors.
+            birthtime(float): Time of birth. The default is time.time()
+            name(None of string): Name of the individual. The default is sha256 from the time of birth.
+        """
         super(BaseIndvdl, self).__init__()
         if name == None:
             self.name = hashlib.sha256(str(time).encode()).hexdigest()
@@ -21,8 +26,9 @@ class BaseIndvdl(nn.ModuleList):
             self.name = str(name)
         self.birthtime = birthtime
         self.eval:tuple = (None,)
+        self.target_tensors = target_tensors
 
-    def freeze_module(self, module):
+    def freeze_module(self, module:nn.Module):
         for parameter in list(module.parameters()):
             parameter.requires_grad = False
         
@@ -42,3 +48,48 @@ class BaseIndvdl(nn.ModuleList):
         pop_module = self[index]
         del self[index]
         return pop_module
+
+    def parameters_zero(self):
+        for i, params in enumerate(self.parameters()):
+            if self.target_tensors == "all":
+                params.zero_()
+            elif i in self.target_tensors:
+                params.zero_()
+
+    def get_len(self):
+        """
+        return count elements of model
+        """
+        len = 0
+        for params in list(self.parameters()):
+            len += torch.tensor(params.shape).prod().item()
+        return len
+
+    def get_val(self,index):
+        if self.get_len()-1 < index or index < 0:
+            raise IndexError(f"IndexError: list index out of range. index must be >=0  and <= {self.get_len()-1}")
+        count_params = 0
+        for params in self.parameters():
+            len_params = torch.tensor(params.data.shape).prod().item()
+            if index == 0 and count_params==0:
+                params_value = params.data.flatten()[index]
+                return params_value.item()
+            count_params += len_params
+            if count_params > index:
+                index_params = index-count_params
+                params_value = params.data.flatten()[index_params-1]
+                return params_value.item()
+
+
+    def set_val(self,index,val):
+        if self.get_len()-1 < index or index < 0:
+            raise IndexError(f"IndexError: list index out of range. index must be >=0  and <= {self.get_len()-1}")
+        count_params = 0
+        for params in self.parameters():
+            len_params = torch.tensor(params.data.shape).prod().item()
+            count_params += len_params
+            if  count_params >= index:
+                index_params = index-count_params
+                params.data.flatten()[index_params-1] = val
+                return True
+        raise IndexError(f"The value {val} is not assigned to index {index}.")
