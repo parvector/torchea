74aed3192b06bf6e938c5390e7d6a8c902faad39
Parviz Mirzoev <parvector@yandex.com>
2023-05-05 10:52:48 +0300

    add cros_DE0 test

--- tests/test_cros.py
+++ tests/test_cros.py
@@ -1,25 +1,62 @@
 import unittest
 from torchea import cros
 from torch import nn
+import torch
 from torchea.base import BaseIndvdl
 
 
 
 class TestCros(unittest.TestCase):
     def test_crosDE0(self):
-        ti0 = BaseIndvdl()
-        ti0.append(nn.Linear(2,2))
-        ti0.parameters_zero()
-        for params in ti0.parameters():
-            params += 0
+        modelx = BaseIndvdl()
+        modelx.append(nn.Linear(4,2))
+        modelx.parameters_zero()
 
-        ti1 = BaseIndvdl()
-        ti1.append(nn.Linear(2,2))
-        ti1.parameters_zero()
-        for params in ti1.parameters():
+        modelv = BaseIndvdl()
+        modelv.append(nn.Linear(4,2))
+        modelv.parameters_zero()
+        for params in modelv.parameters():
             params += 1
 
-        modelu = cros.crosDE0(ti0, ti1, CR=1)
+        modelu = cros.crosDE0(modelx, modelv, CR=0)
+        for params in modelu.parameters():
+            self.assertEqual((params.flatten()==0).all(), True)
+        
+        sum_ones = 0
+        count_tests = 500
+        min_treshold = 0.8
+        max_treshold = 1.2
+        for i in range(count_tests):
+            modelu = cros.crosDE0(modelx, modelv, CR=0.1)
+            count_ones = sum([ (params.data == 1).sum() for params in modelu.parameters() ]).item()
+            sum_ones += count_ones
+        avg_value = sum_ones/count_tests
+        self.assertEqual(min_treshold<avg_value<max_treshold, True)
+        
+
+        sum_ones = 0
+        count_tests = 500
+        min_treshold = 4.8
+        max_treshold = 5.2
+        for i in range(count_tests):
+            modelu = cros.crosDE0(modelx, modelv, CR=0.5)
+            count_ones = sum([ (params.data == 1).sum() for params in modelu.parameters() ]).item()
+            sum_ones += count_ones
+        avg_value = sum_ones/count_tests
+        self.assertEqual(min_treshold<avg_value<max_treshold, True)
+
+        sum_ones = 0
+        count_tests = 500
+        min_treshold = 8.8
+        max_treshold = 9.2
+        for i in range(count_tests):
+            modelu = cros.crosDE0(modelx, modelv, CR=0.9)
+            count_ones = sum([ (params.data == 1).sum() for params in modelu.parameters() ]).item()
+            sum_ones += count_ones
+        avg_value = sum_ones/count_tests
+        self.assertEqual(min_treshold<avg_value<max_treshold, True)
+
+        modelu = cros.crosDE0(modelx, modelv, CR=1)
         for params in modelu.parameters():
             self.assertEqual((params.flatten()==1).all(), True)
 
