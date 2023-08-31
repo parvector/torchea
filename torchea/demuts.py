2d6afbfc4753b475648c26e97e82d4d01bd801de
Parviz Mirzoev <parvector@yandex.com>
2023-08-31 12:01:49 +0300

    edit mut0

--- torchea/demuts.py
+++ torchea/demuts.py
@@ -3,12 +3,13 @@ import torch
 
 
 
-def de0(model1, model2, model3, F):
+def mut0(model1, model2, model3, F):
     """
     Article: Storn, Rainer & Price, Kenneth. (1995). Differential Evolution: A Simple and Efficient Adaptive Scheme for Global Optimization Over Continuous Spaces. Journal of Global Optimization. 23. 
-    """ 
-    modelv = copy.deepcopy(model1)
+    """
+    modelv = model1.deepcopy()
     modelv.parameters_zero()
-    for newv, v1, v2, v3 in zip(modelv.parameters(), model1.parameters(), model2.parameters(), model3.parameters()):
-        newv += v1+F*(v2-v3)
+    with torch.no_grad():
+        for newv, v1, v2, v3 in zip(modelv.parameters(), model1.parameters(), model2.parameters(), model3.parameters()):
+            newv += v1+F*(v2-v3)
     return modelv
