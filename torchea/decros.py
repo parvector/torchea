a981f04ab07423d4822d6f95282e6c18eddfb4ee
Parviz Mirzoev <parvector@yandex.com>
2023-08-31 12:00:30 +0300

    edit name and bit edit func cros0

--- torchea/decros.py
+++ torchea/decros.py
@@ -4,14 +4,14 @@ from torch.distributions.uniform import Uniform
 
 
 
-def de0(modelx, modelv, CR=0.1, 
+def cros0(modelx, modelv, CR=0.1, 
             distribution=Uniform(torch.tensor([0.0]), torch.tensor([1.0]))):
     """
     Article: Storn, Rainer and Price, Kenneth. Differential Evolution — A Simple and Efficient Heuristic for Global Optimization over Continuous Spaces. ournal of Global Optimization 11: 341–359, 1997.
     """
-    
-    modelu = copy.deepcopy(modelx)
-    modelu_count_ws = modelx.count_ws()
+        
+    modelu = modelx.deepcopy()
+    modelu_count_ws = modelu.count_ws()
     for j in range(modelu_count_ws):
         if torch.rand(1).item() <= CR or j == distribution.sample().item():
             modelu.setv(j, modelv.getv(j) )
