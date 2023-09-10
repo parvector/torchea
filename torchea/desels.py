e691ec6b8d5b5561f73671ebd2e9b266b94eded7
parvector <parvector@yandex.com>
2023-09-10 21:03:47 +0300

    rename eval to fitnes

--- torchea/desels.py
+++ torchea/desels.py
@@ -8,23 +8,23 @@ def softsel(modelu, modelx, if_both_eq="first"):
     if not (if_both_eq == "both" or if_both_eq == "first" or if_both_eq == "second"):
         raise ValueError("if_both_eq must be 'both', 'first', 'second'")
 
-    if all([ ueval == xeval for ueval, xeval in zip(modelu.eval, modelx.eval) ]) and if_both_eq == "both":
+    if all([ ufitnes == xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes) ]) and if_both_eq == "both":
         return modelu, modelx
-    elif all([ ueval == xeval for ueval, xeval in zip(modelu.eval, modelx.eval) ]) and if_both_eq == "first":
+    elif all([ ufitnes == xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes) ]) and if_both_eq == "first":
         return modelu
-    elif all([ ueval == xeval for ueval, xeval in zip(modelu.eval, modelx.eval) ]) and if_both_eq == "second":
+    elif all([ ufitnes == xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes) ]) and if_both_eq == "second":
         return modelx
-    elif all([ ueval <= xeval for ueval, xeval in zip(modelu.eval, modelx.eval) ]) \
-        and not all([ ueval == xeval for ueval, xeval in zip(modelu.eval, modelx.eval) ]):
+    elif all([ ufitnes <= xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes) ]) \
+        and not all([ ufitnes == xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes) ]):
         return modelx
-    elif all([ ueval >= xeval for ueval, xeval in zip(modelu.eval, modelx.eval) ]) \
-        and not all([ ueval == xeval for ueval, xeval in zip(modelu.eval, modelx.eval) ]):
+    elif all([ ufitnes >= xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes) ]) \
+        and not all([ ufitnes == xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes) ]):
         return modelu
     
 def hardsel(modelu, modelx):
-    if all([ ueval > xeval for ueval, xeval in zip(modelu.eval, modelx.eval)]):
+    if all([ ufitnes > xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes)]):
         return modelu
-    elif all([ ueval < xeval for ueval, xeval in zip(modelu.eval, modelx.eval)]):
+    elif all([ ufitnes < xfitnes for ufitnes, xfitnes in zip(modelu.fitnes, modelx.fitnes)]):
         return modelx
     else:
         return False
